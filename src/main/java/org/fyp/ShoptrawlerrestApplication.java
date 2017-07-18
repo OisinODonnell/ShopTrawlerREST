5b0ec41f520e27b7be669b1e93d0eaf6676a47ea
Oisin-HP\Oisin <oisinodonnell6@gmail.com>
2017-07-18 14:30:16 +0100

    Created Projected with Spring Initializer.
    Made changes to the Pom File, with regards the database and dependencies to be brought in.
    
    Filled out the application.properties file - sets the URL to the database (incl. the username and password) and the action to CREATE on load instead of UPDATE.

---
+++ src/main/java/org/fyp/ShoptrawlerrestApplication.java
@@ -0,0 +1,12 @@
+package org.fyp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoptrawlerrestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoptrawlerrestApplication.class, args);
+	}
+}
